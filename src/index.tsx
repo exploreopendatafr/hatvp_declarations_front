74257365b5fdfbba84ad24068f4142e500a4e7ea
azro352 <raphi.352@hotmail.fr>
2023-03-20 22:20:04 +0100

    rm: useless files. fix:base route path

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import { ConfigProvider, theme } from "antd";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="hatvp_declarations_front">
             <ConfigProvider
                 theme={{
                     algorithm: theme.darkAlgorithm,
@@ -24,8 +23,3 @@ root.render(
         </BrowserRouter>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
